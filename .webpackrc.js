e754f95c7849ed392828706513360d878afc9231
ruanwenfeng <791113844@qq.com>
2018-04-23 16:02:21 +0800

    init
    Signed-off-by: ruanwenfeng <ruanwenfeng@vusion.com.cn>

---
+++ .webpackrc.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+      publicPath: '/',
+    },
+    production:{
+      publicPath: '.',
+      outputPath: './static',
+    }
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
+  },
+  disableDynamicImport: true,
+  hash: true,
+};
